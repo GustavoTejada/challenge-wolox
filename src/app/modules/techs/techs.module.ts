ee832c97c60e99f5e93b46731a71ba2167850212
GustavoTejada <45311587+GustavoTejada@users.noreply.github.com>
2021-06-04 15:54:59 -0300

    generated modules
    se generaron los siguientes módulos:
    - Shared
    - Core
    
    y se creó al carpeta Modules

---
+++ src/app/modules/techs/techs.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TechsRoutingModule } from './techs-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TechsRoutingModule
+  ]
+})
+export class TechsModule { }
